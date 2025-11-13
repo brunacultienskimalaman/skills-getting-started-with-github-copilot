449d8e9a562250f32bc22521d724cfd80a0831bf
Bruna Cultienski Malaman <brunacultienski@gmail.com>
2025-11-13 19:23:12 +0000

    Add validation to prevent duplicate sign-ups for activities

--- src/app.py
+++ src/app.py
@@ -55,7 +55,12 @@ def get_activities():
 @app.post("/activities/{activity_name}/signup")
 def signup_for_activity(activity_name: str, email: str):
     """Sign up a student for an activity"""
-    # Validate activity exists
+    
+    # Validate student is not already signed up 
+    for activity in activities.values():
+        if email in activity["participants"]:
+            raise HTTPException(status_code=400, detail="Student already signed up for an activity")
+
     if activity_name not in activities:
         raise HTTPException(status_code=404, detail="Activity not found")
 
